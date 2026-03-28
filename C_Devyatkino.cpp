a4cedb7d798793214bf0efd1630e9fa310eccfd1
Rafsan Riasat <riasat1011@gmail.com>
2026-03-28 20:42:57 +0600

    day 31

--- C_Devyatkino.cpp
+++ C_Devyatkino.cpp
@@ -1,57 +1,70 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define int long long
+#define double long double
+#define endl '\n'
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define sp " "
+#define Flashyy ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define tc int TC;cin>>TC;for(int tt=1;tt<=TC;tt++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) x.size()
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
 
-vector<long long> s;
-
-void precompute() {
-    s.clear();
-    long long num = 0;
-    for (int digits = 1; digits <= 18; digits++) {
-        num = num * 10 + 9;
-        s.push_back(num);
-    }
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
 }
 
-bool has7(long long x) {
-    string str = to_string(x);
-    return str.find('7') != string::npos;
-}
+int32_t main(){
+    Flashyy
+    tc
+    {
+        int n;
+        cin>>n;
 
-int main() {
-    precompute();
-    int t;
-    cin >> t;
-    while (t--) {
-        long long n;
-        cin >> n;
-        string str_n = to_string(n);
-        if (str_n.find('7') != string::npos) {
-            cout << 0 << endl;
-            continue;
-        }
-        bool found = false;
-        for (long long x : s) {
-            long long sum = n + x;
-            if (has7(sum)) {
-                cout << 1 << endl;
-                found = true;
+        for(int k=0;k<=7;k++){
+            int m=n-k;
+            if(m<0)break;
+            string s=to_string(m);
+
+            if(s.find('7')!=string::npos){
+                cout<<k<<endl;
+                break;
+            }
+
+            s="0"+s;
+            bool f=0;
+
+            for(int i=0;i<sz(s);i++){
+                int dig=s[i]-'0';
+                int need=(17-dig)%10;
+                if(need<=k){
+                    f=1;
+                    break;
+                }
+            }
+
+            if(f){
+                cout<<k<<endl;
                 break;
             }
         }
-        if (found) continue;
-        for (long long x : s) {
-            for (long long y : s) {
-                long long sum = n + x + y;
-                if (has7(sum)) {
-                    cout << 2 << endl;
-                    found = true;
-                    goto next_case;
-                }
-            }
-        }
-        next_case:
-        if (found) continue;
-        cout << 3 << endl;
     }
-    return 0;
+return 0;
 }
